e0440be8fcc4547587468591a915598708ac3418
Ben Werner <42802102+benwerner01@users.noreply.github.com>
2022-07-10 18:24:27 +0100

    Home page redesign

---
+++ components/ChevronRightIcon.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+export const ChevronRightIcon: FC<JSX.IntrinsicElements["svg"]> = (props) => (
+  <svg
+    width="28"
+    height="28"
+    viewBox="0 0 28 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M10.7564 18.0127L14.7691 14L10.7564 9.98725L11.9936 8.75L17.2436 14L11.9936 19.25L10.7564 18.0127V18.0127Z"
+      fill="currentColor"
+    />
+  </svg>
+);
